6f3c29f18e041b6882f44744d14cb6577f5e7838
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-05 18:15:42 -0400

    create About.js, import and add its path in app.js to get info about store

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import Books from "./components/Book/Books";
+import About from "./components/About";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} exact />
         <Route path="/add" element={<AddBook />} exact />
         <Route path="/books" element={<Books />} exact />
+        <Route path="/about" element={<About />} exact />
         </Routes>
       </main>
     </React.Fragment>
---
+++ book-store/src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>This is the About us page</div>
+  )
+}
+
+export default About
