6e824af0d28ac023c6e7ffc16327a7a4f770af66
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-05 07:35:05 -0400

    connect vs to mongo datbase

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+
+// added for port
+require('dotenv').config()
+// Identify our port
+const port = process.env.PORT
+
+mongoose.connect(process.env.MONGO_DB);
+mongoose.connection.once('open', () =>
+{
+    console.log('Connected to MongoDB')
+})
+
+// Listen to port
+app.listen(port, () => {
+    console.log('Listening on port: ', port)
+})
+
