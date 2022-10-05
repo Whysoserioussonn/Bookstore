94aafee7ed12624d2c2cf97929c6bdb4ff589054
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-05 15:55:26 -0400

    update index.js  to add nav links to tab headers add product, books and about us

--- book-store/src/components/Header.js
+++ book-store/src/components/Header.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { AppBar, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
+import { NavLink } from 'react-router-dom'
+
 const Header = () => {
 
     const [value, setValue] = useState();
@@ -16,11 +18,11 @@ const Header = () => {
          indicatorColor="secondary"
          value={value}
          onChange={(e,val) =>setValue(val)} 
-         >
+         > 
         
-          <Tab label='Add Product'/>
-          <Tab label='Books' />
-          <Tab label='About Us' />
+          <Tab LinkComponent={NavLink} to="/add" label='Add Product'/>   
+          <Tab LinkComponent={NavLink} to="/books" label='Books' />
+          <Tab LinkComponent={NavLink} to="/about" label='About Us' />
         </Tabs>
         
         </Toolbar>
