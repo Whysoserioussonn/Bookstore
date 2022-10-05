0dccdba6905d09658d5a47b75f0873a565301caa
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-05 12:16:42 -0400

    added delete book function  with exports, router added to bookroutes

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -9,7 +9,7 @@ router.get("/", booksController.getAllBooks);
 router.post("/", booksController.addBook);
 router.get("/:id", booksController.getById);
 router.put("/:id", booksController.updateBook);
-
+router.delete("/:id", booksController.deleteBook);
 
 
 module.exports = router
