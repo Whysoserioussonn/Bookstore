21627bdf76f1cdd17abee57c179f9c575d1e5372
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-05 22:40:31 -0400

    added an about section and styled it

--- book-store/src/components/About.js
+++ book-store/src/components/About.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div>This is the About us page</div>
-  )
-}
+    <div className="About">
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h2">
+          
+        </Typography>
+        <Typography sx={{ fontFamily: "fantasy" }} variant="h3">
+          This site was designed as a <br></br>backend project for Per Scholas. <br></br>  It was upgraded to include
+          a front end with React. <br></br> User is able to Create, Read, Update, and Delete items from bookstore.<br></br>
+          Hope you enjoy the site!<br></br>
+          Per Scholas Fall Class of 2022<br></br>
+          Henry Murillo   
+        </Typography>
+      </Box>
+    </div>
+  );
+};
 
-export default About
+export default About;
