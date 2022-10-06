af8da93a4988d221df74508f3c04a59a4707961a
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-06 09:36:32 -0400

    updated about page with star wars theme

--- book-store/src/components/About.js
+++ book-store/src/components/About.js
@@ -4,18 +4,26 @@ import "./About.css";
 
 const About = () => {
   return (
-    <div id="About">
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <Typography sx={{ fontFamily: "fantasy" }} variant="h2">
-        This site was designed as a <br></br>backend project for Per Scholas. <br></br>  It was upgraded to include
-          a front end with React. <br></br> User is able to Create, Read, Update, and Delete items from bookstore website.<br></br>
-          Hope you enjoy!<br></br>
-          Per Scholas Fall Class of 2022<br></br>
-          Henry Murillo  
-        </Typography>
-       
-      </Box>
-    </div>
+    <><div class="fade"></div><section class="star-wars">
+
+          <div class="crawl">
+
+              <div class="title">
+                  <p>Project 2 </p>
+                  <h1>The Market Website</h1>
+              </div>
+
+              <p>After surviving the first project where the objective was to make a video game. We have moved to the 2nd phase, the market website store.</p>
+              <p>This site was designed as a backend project for Per Scholas. It was upgraded to include a front end with React.</p>
+              <p>User is able to Create, Read, Update, and Delete items from bookstore website. They can navigate to all books index from any screen, an about us and , also if they want to add a new book to the library</p>
+              <p>User also has the option to edit and delete a book from the webiste using a form.</p>
+              <p>Hope you enjoy!</p>
+              <p>Per Scholas Fall Class of 2022</p>
+              <p>Henry Murillo</p>
+
+          </div>
+
+      </section></>
   );
 };
 
